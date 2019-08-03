63a812eb88cbddaf2e5115ee81a492ef8bdaa411
Keisuke Arai <usd1122334@gmail.com>
2019-08-03 17:56:32 +0900

    first commit

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -8,21 +8,14 @@ class FavoritesController < ApplicationController
     favorite = Favorite.new
     favorite.user_id = current_user.id
     favorite.topic_id = params[:topic_id]
-
-    if favorite.save
-      redirect_to topics_path, success: 'お気に入りに登録しました'
-    else
-      redirect_to topics_path, danger: 'お気に入り登録に失敗しました'
-    end
+    favorite.save
+    @topic = Topic.find_by(id: params[:topic_id])
   end
 
-  def delete
+  def destroy
     favorite = Favorite.find_by(user_id: current_user.id, topic_id: params[:topic_id])
     # binding.pry
-    if favorite.destroy
-      redirect_to topics_path, success: '削除しました'
-    else
-      redirect_to topics_path, danger: '失敗しました'
-    end
+    favorite.destroy
+    @topic = Topic.find_by(id: params[:topic_id])
   end
 end
